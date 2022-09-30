4064324e05ba0363fbd7ecd4b6063d14c87dc80b
Кирилл <@LAPTOP-LV6EI7UV>
2022-09-30 20:11:41 +0300

    Добавьте файлы проекта.

---
+++ KTPO4311.Osanov.Lib/src/LogAn/LogAnalyzer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KTPO4311.Osanov.Lib.src.LogAn
+{
+    public class LogAnalyzer
+    {
+        public bool WasLastFileNameValid { get; set; }
+        public bool IsValidLogFileName(string fileName)
+        {
+            WasLastFileNameValid = false;
+            if (string.IsNullOrEmpty(fileName))
+            {
+                throw new ArgumentException("имя файла должно быть задано");
+            }
+            if (!fileName.EndsWith(".OKG",StringComparison.CurrentCultureIgnoreCase))
+            {
+                return false;
+            }
+            WasLastFileNameValid=true;
+            return true;
+        }
+    }
+}
---
+++ KTPO4311.Osanov.UnitTest/src/LogAn/LogAnalyzerTests.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using KTPO4311.Osanov.Lib.src.LogAn;
+
+namespace KTPO4311.Osanov.UnitTest.src.LogAn
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [Test]
+        public void IsValidFileName_BadExtension_ReturnFalse()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer.IsValidLogFileName("filewithbadextension.foo");
+            Assert.False(result);
+        }
+        [Test]
+        public void IsValidLogFileName_GoodExtensionUppercase_ReturnTrue()
+        {
+            LogAnalyzer analyzer= new LogAnalyzer();
+            bool result = analyzer.IsValidLogFileName("fileName.OKG");
+            Assert.True(result);
+        }
+        [Test]
+        public void IsValidLogName_GoodExtensionLowercase_ReturnTrue()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer.IsValidLogFileName("fileName.okg");
+            Assert.True(result);
+        }
+        [TestCase("fileName.OKG")]
+        [TestCase("fileName.okg")]
+        public void IsValidLogName_ValidExtension_ReturnsTrue(string file)
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer.IsValidLogFileName(file);
+            Assert.True(result);
+        }
+        [Test]
+        public void IsValidFileName_EmptyFileName_Throws()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            var ex = Assert.Catch<Exception>(() => analyzer.IsValidLogFileName(""));
+            StringAssert.Contains("имя файла должно быть задано", ex.Message);
+        }
+        [TestCase("badfile.foo",false)]
+        [TestCase("goodfile.OKG",true)]
+        public void IsValidFileName_WhenCalled_ChangesWasLastFileNameValid(string file,bool expected)
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            analyzer.IsValidLogFileName(file);
+            Assert.AreEqual(expected, analyzer.WasLastFileNameValid);
+        }
+    }
+}
